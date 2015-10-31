bdd3cbb4a942f13a2ef1e4ad955428c9ac86e10d
abao <talent518@yeah.net>
2015-10-31 18:29:23 +0000

    解决头文件中忘记加inline问题。

--- hook.h
+++ hook.h
@@ -8,7 +8,7 @@
 
 inline bool hook_conn_accept(conn_t *ptr);
 inline void hook_conn_close(conn_t *ptr);
-void hook_conn_recv(conn_t *ptr,const char *data, int data_len);
+inline void hook_conn_recv(conn_t *ptr,const char *data, int data_len);
 inline void hook_conn_denied(conn_t *ptr);
 
 inline void hook_thread_init(worker_thread_t *thread);
